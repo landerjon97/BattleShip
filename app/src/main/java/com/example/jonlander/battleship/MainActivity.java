dc4e2fdb338b7af7034e978be51a0e35027a7ce7
jlander <lander.jon9@gmail.com>
2017-10-10 10:20:33 -0400

    Whats new: 2 new activities fullScreen(sort of) 2 buttons that go to activities

--- app/src/main/java/com/example/jonlander/battleship/MainActivity.java
+++ app/src/main/java/com/example/jonlander/battleship/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.jonlander.battleship;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setting a listener up for the button to go to new activity
+        (findViewById(R.id.start)).setOnClickListener(this);
+        (findViewById(R.id.credits)).setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        //when the start button is pressed go to different activity (switch statement)
+        switch(v.getId()){
+            case R.id.start:
+                Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameIntent);
+                break;
+            case R.id.credits:
+                Intent creditIntent = new Intent(MainActivity.this, CreditActivity.class);
+                startActivity(creditIntent);
+
+        }
     }
 }
